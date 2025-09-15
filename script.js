e174b2d3cc7ee0a31bcc477d59d518649647155b
matiasjulia <matias.fonseca@escola.pr.gov.br>
2025-09-15 11:53:26 -0300

    script

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+ function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
-function mostraPalavrasChave() { 
-  let palavras = texto.split(/\P{L}+/u);
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
 
     for (let i in palavras) {
         palavras[i] = palavras[i].toLowerCase();
@@ -14,4 +20,31 @@ function mostraPalavrasChave() {
 
     const frequencias = contaFrequencias(palavras);
 
-} 
+function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
+function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+
+
+function tiraPalavrasRuins(palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
+}
+
+
+   
+
