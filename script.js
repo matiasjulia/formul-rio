f946946b663a951627a09e55f146a8c20eab7938
matiasjulia <matias.fonseca@escola.pr.gov.br>
2025-09-15 11:20:24 -0300

    script

---
+++ script.js
@@ -0,0 +1,17 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
+function mostraPalavrasChave() { 
+  let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+
+} 
